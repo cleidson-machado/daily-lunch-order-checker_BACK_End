4525f14318aa9e1f81210ae93d0124b8c5f9c9f0
Cleidson <cleidson.mac@outlook.com>
2023-01-28 15:21:39 -0400

    THE SCHEMA Only... And the Clean PROJECT

---
+++ prisma/migrations/20230128191524_recreate_user_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `idCompanyEmployee` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `passWord` VARCHAR(191) NOT NULL,
+    `isActive` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
